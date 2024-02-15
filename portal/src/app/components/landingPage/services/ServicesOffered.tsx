2b7dd3a8890780e472457fbeb0a19099239a22c2
Abdul <abdulfaim393@gmail.com>
2024-02-15 23:53:18 +0530

    landing page css rectification

--- portal/src/app/components/landingPage/services/ServicesOffered.tsx
+++ portal/src/app/components/landingPage/services/ServicesOffered.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { FC } from "react";
-import { Box, Grid, Theme, Typography } from "@mui/material";
+import { Box, Container, Grid, Theme, Typography } from "@mui/material";
 import { makeStyles } from "tss-react/mui";
 import webdev from "../../../../../public/webdev.svg";
 import mobileDev from "../../../../../public/mobileDev.svg";
@@ -13,58 +13,107 @@ import Offeredservice from "./offeredservice";
 
 const useStyles = makeStyles()((_theme: Theme) => ({
   root: {},
+  service_offered_box: {
+    paddingTop: "25px",
+    paddingBottom: "25px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+  },
+  service_h3: {
+    fontSize: "2rem",
+    fontWeight: 500,
+  },
+  service_h2: {
+    fontSize: "2.5rem",
+    fontWeight: 600,
+  },
 }));
 export interface ServicesOfferedProps {
   text?: string;
 }
 
 const services = [
-  { img: webdev, main: "Web Development", body: "We develop websites and applications that are easy to use, visually appealing, and accessible to a wide range of devices." },
-  { img: mobileDev, main: "Mobile Development", body: "We provide best-in-class mobile app development services focusing on drafting concepts and developing truly successful apps." },
-  { img: dm, main: "Digital Marketing", body: "We offer comprehensive digital marketing solutions to enhance your online presence and reach your target audience effectively." },
-  { img: gps, main: "Vehicle GPS Tracking", body: "We provide accurate vehicle GPS tracking services to monitor vehicle location, direction, speed, and more." },
-  { img: load, main: "Vehicle Load Monitoring System", body: "Our vehicle load monitoring system provides real-time data on vehicle location, direction, time, and speed." },
-  { img: fuel, main: "Fuel Management", body: "We offer fuel management solutions to monitor fuel usage, optimize fuel consumption, and reduce costs." }
-]
+  {
+    img: webdev,
+    main: "Web Development",
+    body: "We develop websites and applications that are easy to use, visually appealing, and accessible to a wide range of devices.",
+  },
+  {
+    img: mobileDev,
+    main: "Mobile Development",
+    body: "We provide best-in-class mobile app development services focusing on drafting concepts and developing truly successful apps.",
+  },
+  {
+    img: dm,
+    main: "Digital Marketing",
+    body: "We offer comprehensive digital marketing solutions to enhance your online presence and reach your target audience effectively.",
+  },
+  {
+    img: gps,
+    main: "Vehicle GPS Tracking",
+    body: "We provide accurate vehicle GPS tracking services to monitor vehicle location, direction, speed, and more.",
+  },
+  {
+    img: load,
+    main: "Vehicle Load Monitoring System",
+    body: "Our vehicle load monitoring system provides real-time data on vehicle location, direction, time, and speed.",
+  },
+  {
+    img: fuel,
+    main: "Fuel Management",
+    body: "We offer fuel management solutions to monitor fuel usage, optimize fuel consumption, and reduce costs.",
+  },
+];
 const ServicesOffered: FC<ServicesOfferedProps> = (props) => {
   const { classes } = useStyles();
 
   return (
     <>
       <Box
-        sx={{
-          marginTop: "700px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          width: "100%",
-          borderRadius: "10px",
-          boxShadow: "0 0 10px rgba(0,0,0,0.1)",
-        }}
+        className={classes.service_offered_box}
+        // sx={{
+        //   marginTop: "700px",
+        //   paddingTop: "25px",
+        //   paddingBottom: "25px",
+        //   display: "flex",
+        //   flexDirection: "column",
+        //   justifyContent: "center",
+        //   alignItems: "center",
+        //   height: "100%",
+        //   width: "100%",
+        //   borderRadius: "10px",
+        //   boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+        // }}
       >
-        <Typography variant="h3" color="initial">
+        <Typography variant="h3" color="initial" className={classes.service_h3}>
           WHAT WE CAN DO FOR YOU
         </Typography>
-        <Typography variant="h2" color="initial">
+        <Typography variant="h2" color="initial" className={classes.service_h2}>
           SEVICES WE CAN HELP YOU WITH
         </Typography>
-        <Grid
-          container
-          spacing={1}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          alignContent="stretch"
-          wrap="wrap"
-        >
-           {services.map(({ img, main, body }) => (
-          <Grid key={main} item xs={12} sm={6} md={4} lg={3}>
-            <Offeredservice img={img} main={main} body={body} />
+        <Container maxWidth="xl">
+          <Grid
+            container
+            spacing={1}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            alignContent="stretch"
+            wrap="wrap"
+          >
+            {services.map(({ img, main, body }) => (
+              <Grid key={main} item xs={12} sm={6} md={4} lg={4} xl={4}>
+                <Offeredservice img={img} main={main} body={body} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-        </Grid>
+        </Container>
       </Box>
     </>
   );
--- portal/src/app/components/landingPage/topbar/banner/Banner.tsx
+++ portal/src/app/components/landingPage/topbar/banner/Banner.tsx
@@ -2,10 +2,18 @@ import React, { FC } from "react";
 import { Theme, Grid, Box, Typography } from "@mui/material";
 import { makeStyles } from "tss-react/mui";
 import Image from "next/image";
-import banner from '../../../../../../public/banner1.svg'
+import banner from "../../../../../../public/banner1.svg";
 
-const useStyles = makeStyles()((_theme: Theme) =>({
-  root:{}
+const useStyles = makeStyles()((_theme: Theme) => ({
+  root: {},
+  banner_image: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+  },
+  banner_box:{
+    marginTop: "100px",
+  }
 }));
 
 export interface BannerProps {
@@ -16,20 +24,18 @@ const Banner: FC<BannerProps> = (props) => {
   const { classes } = useStyles();
 
   return (
-    <Box
-      width="100%" 
-      display="flex"
-      justifyContent="center" 
-      alignItems="center"
-      margin={0} 
-      padding={0}  
-    >
-      <Image
-        src={banner}
-        alt="banner1"
-        layout="fill" 
-      />
+    // <Box
+    //   width="100%"
+    //   display="flex"
+    //   justifyContent="center"
+    //   alignItems="center"
+    //   margin={0}
+    //   padding={0}
+    // >
+    <Box className={classes.banner_box}>
+      <Image src={banner} alt="banner1" className={classes.banner_image} />
     </Box>
+    // </Box>
   );
 };
 
